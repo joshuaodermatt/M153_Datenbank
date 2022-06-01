eab7da03014ff9f77c9f0ae2f9504d5f2e28f6a9
Lian Aeschlimann <lian_aeschlimann@sluz.ch>
2022-06-01 14:06:13 +0200

    added create database (30min)

---
+++ create.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE BierLexikon;
+USE BierLexikon;
+
+CREATE TABLE BierLiebhaber
+(
+    BierLiebhaberId int NOT NULL ,
+    Name varchar(45) NOT NULL ,
+    Geburtsdatum date NOT NULL ,
+    PRIMARY KEY (BierLiebhaberId)
+);
+
+CREATE TABLE BierTyp
+(
+    BierTypId int NOT NULL ,
+    Name varchar(45) NOT NULL ,
+    PRIMARY KEY (BierTypId)
+);
+
+
+CREATE TABLE Marken
+(
+    MarkenId int NOT NULL ,
+    Name varchar(45) NOT NULL ,
+    Gruendungsjahr smallint NOT NULL ,
+    Standort varchar(45) NOT NULL ,
+    PRIMARY KEY (MarkenId)
+);
+
+CREATE TABLE Biersorten
+(
+    BierstortenId int NOT NULL ,
+    Name varchar(45) NOT NULL ,
+    Fk_BierTypId int NOT NULL ,
+    Fk_MarkenId int NOT NULL ,
+    Veroefentlichungsdatum date NOT NULL ,
+    Alkoholfrei bit NOT NULL ,
+    Alkoholgehalt float NOT NULL ,
+    PRIMARY KEY (BierstortenId),
+    CONSTRAINT FK_BiersortenMarke FOREIGN KEY (FK_MarkenId) REFERENCES Marken (MarkenId),
+    CONSTRAINT FK_BiersortenTyp FOREIGN KEY (FK_BierTypId) REFERENCES BierTyp (BierTypId)
+);
+
+
+CREATE TABLE BierFavorit
+(
+    BierFavoritId int NOT NULL ,
+    BierLiebhaberId int NOT NULL ,
+    BierstortenId int NOT NULL ,
+    PRIMARY KEY (BierFavoritId),
+    CONSTRAINT FK_BierLiebhaberFavorit FOREIGN KEY (BierLiebhaberId) REFERENCES BierLiebhaber(BierLiebhaberId),
+    CONSTRAINT FK_BierFavoritSorte FOREIGN KEY (BierstortenId) REFERENCES Biersorten(BierstortenId)
+);
