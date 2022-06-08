8e43d69d1c99e9c077c93c267f13569391c8f766
Lian Aeschlimann <lian_aeschlimann@sluz.ch>
2022-06-08 10:58:53 +0200

    added sp_DurchschnitAlkoholgehaltVon procedure
    40min

---
+++ procedure.sql
@@ -0,0 +1,20 @@
+
+DROP PROCEDURE IF EXISTS sp_DurchschnitAlkoholgehaltVon;
+
+DELIMITER //
+CREATE PROCEDURE sp_DurchschnitAlkoholgehaltVon
+ (IN marke varchar(45), OUT average float )
+BEGIN 
+	SELECT AVG(Alkoholgehalt) 'Alkoholgehalt Durchschnitt'
+    INTO average FROM Biersorten 
+    WHERE Fk_MarkenId = (SELECT MarkenId from Marken WHERE Name = marke);
+END //
+
+DELIMITER ;
+
+CALL sp_DurchschnitAlkoholgehaltVon('Corona', @average);
+SELECT @average;
+
+
+
+
