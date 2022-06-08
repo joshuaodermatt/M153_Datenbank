bc7c48daae51aca7be6c45baf99bdecb9a346f47
Lian Aeschlimann <lian_aeschlimann@sluz.ch>
2022-06-08 12:25:57 +0200

    swichet delimiter

--- procedure.sql
+++ procedure.sql
@@ -1,14 +1,14 @@
 
 DROP PROCEDURE IF EXISTS sp_DurchschnitAlkoholgehaltVon;
 
-DELIMITER //
+DELIMITER $$
 CREATE PROCEDURE sp_DurchschnitAlkoholgehaltVon
  (IN marke varchar(45), OUT average float )
 BEGIN 
 	SELECT AVG(Alkoholgehalt) 'Alkoholgehalt Durchschnitt'
     INTO average FROM Biersorten 
     WHERE Fk_MarkenId = (SELECT MarkenId from Marken WHERE Name = marke);
-END //
+END $$
 
 DELIMITER ;
 
