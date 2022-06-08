9982621306e835c327fc0330caeac93f21b98b58
Lian Aeschlimann <lian_aeschlimann@sluz.ch>
2022-06-08 13:24:13 +0200

    added check if marke exists | 20min

--- procedure.sql
+++ procedure.sql
@@ -5,9 +5,17 @@ DELIMITER $$
 CREATE PROCEDURE sp_DurchschnitAlkoholgehaltVon
  (IN marke varchar(45), OUT average float )
 BEGIN 
-	SELECT AVG(Alkoholgehalt) 'Alkoholgehalt Durchschnitt'
-    INTO average FROM Biersorten 
-    WHERE Fk_MarkenId = (SELECT MarkenId from Marken WHERE Name = marke);
+	DECLARE error_msg varchar(100);
+	SET error_msg = 'Error while trying to calculate average alcohol procentage. Marke does not exist.';
+    
+	IF NOT EXISTS (SELECT MarkenId from Marken WHERE Name = marke)
+	THEN 
+		signal sqlstate '45000' set message_text = error_msg;
+	ELSE
+		SELECT AVG(Alkoholgehalt) 'Alkoholgehalt Durchschnitt'
+		INTO average FROM Biersorten 
+		WHERE Fk_MarkenId = (SELECT MarkenId from Marken WHERE Name = marke);
+	END IF;
 END $$
 
 DELIMITER ;
